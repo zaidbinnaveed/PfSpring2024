28dd585db656ce531959ba5badcf4b98ffedf398
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-02-06 12:23:36 +0500

    Create Task 5.c

---
+++ Lab 2/Task 5.c
@@ -0,0 +1,15 @@
+* Programmer: Zaid Bin Naveed
+* Date: 6th February 2024
+
+#include <stdio.h>
+
+int main() {
+int a,f;
+printf("Enter the temperature in celsius: ");
+scanf("%d",&a);
+
+f=(a * 9/5)+32;
+printf("The temperature in farenheight:%d\n ",f);
+printf("The temperature in celsius:%d ",a);
+return 0;
+}
