e32acd036f75d73044686c08e6dfee74c4dd21bd
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-02-06 16:20:02 +0500

    Create Task 1.c

---
+++ Lab 2/task 1.c
@@ -0,0 +1,13 @@
+* Programmer: Zaid Bin Naveed
+* Date: 6th February 2024
+
+#include <stdio.h>
+int main(){
+    
+int a=5;
+float b=6.5;
+int sum=a+b;
+printf("The sum of the two variables is:%d",sum);
+
+return 0;
+}
