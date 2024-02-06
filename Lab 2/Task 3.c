937ebcff962b78552233bef434c7ee87dd06e4ed
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-02-06 12:22:41 +0500

    Create Task 3.c

---
+++ Lab 2/Task 3.c
@@ -0,0 +1,18 @@
+* Programmer: Zaid Bin Naveed
+* Date: 6th February 2024
+
+
+#include <stdio.h>
+
+int main() {
+int a,b;
+printf("Enter your first number: ");
+scanf("%d",&a);
+
+if(a%2==0){
+    printf("%d is even!",a);
+}
+else{
+    printf("%d is odd!",b);
+}
+}
