1319154fe60b8a11a0c8b3a3cfaafa4845552344
zaidbinnaveed <142867727+zaidbinnaveed@users.noreply.github.com>
2024-03-12 14:45:05 +0500

    Create Task 02.c

---
+++ Lab 6 & 7/Task 02.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+    int choice,n;
+    int nsp=7/2;
+    int nst=1;
+    int ml = 7/2+1;
+    printf("Welcome to the Pattern Printing Game!\n");
+    printf("Enter your choice of pattern you want to print(1 for Triangle)(2 for Pyramid)(3 for Diamond): ");
+    scanf("%d",&choice);
+
+    switch (choice)
+    {
+    case 1:
+    for(int i =1;i<=4;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }   
+    break;
+    case 2:
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=2*i-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    break;
+    case 3:
+    for (int i=1;i<=7;i++)
+    {
+        for(int j=1;j<=nsp;j++)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=nst;k++)
+        {
+            printf("*");
+        }
+        if(i<ml)
+        {
+            nsp--;
+            nst+=2;
+        }
+        else
+        {
+            nsp++;
+            nst-=2;
+        }
+        printf("\n"); 
+    }  
+    break; 
+    default:
+    printf("Wrong choice entered! Choose again!");
+        break;
+    }
+    return 0;
+}
